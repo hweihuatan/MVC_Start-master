028c4a092ebcc462fe445be77eca9235a8b2794c
Hwei Hua Tan <hweihuatan@mail.usf.edu>
2020-02-28 13:10:38 -0500

    Add project files.

---
+++ MVC_Start/Controllers/MyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Start.Models;
+
+namespace MVC_Start.Controllers
+{
+    public class MyController : Controller
+    {
+        public IActionResult Edu()
+        {
+            Education eduInfo = new Education();
+            eduInfo.University = "University of South Florida";
+            eduInfo.StartYear = "2015";
+            eduInfo.ExpectedGraduation = "2021";
+
+            ViewData["Message"] = "My education page.";
+
+            return View(eduInfo);
+        }
+    }
+}
